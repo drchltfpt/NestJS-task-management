4412d9117ebc07c15dfde95dd38fc62c725af790
Pham Huy Ha <ha.phamhuy@vietis.vn>
2019-10-22 14:38:26 +0700

    Implement CORS

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
+  const serverConfig = config.get('server');
+
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
-  logger.log(`Application listening on port ${port}`)
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
